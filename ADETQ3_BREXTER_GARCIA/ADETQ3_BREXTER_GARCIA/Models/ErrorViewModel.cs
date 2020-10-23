b18522b2c91699152940c97949457388ee148e56
BREXTER GARCIA <1788-18@itmlyceumalabang.onmicrosoft.com>
2020-10-23 13:29:23 +0800

    quiz3

---
+++ ADETQ3_BREXTER_GARCIA/ADETQ3_BREXTER_GARCIA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_BREXTER_GARCIA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
